cc1183fe61212dc14dd4a0fd1296b7a6131b7125
Alexander William <alexwilliam112@gmail.com>
2024-08-01 09:43:56 +0700

    bug: db connection

--- server/app.js
+++ server/app.js
@@ -1,17 +1,18 @@
 if (process.env.NODE_ENV !== "production") {
+  console.log('Environment is not production');
   require("dotenv").config();
 }
 
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
-const {  } = require("./schemas/user");
 
 const { connect, getDB } = require("./config/mongo-connection");
 const { authentication } = require("./middlewares/authentication");
+const { userTypes, userResolvers } = require("./schemas/user");
 
 const server = new ApolloServer({
-  typeDefs: [],
-  resolvers: [],
+  typeDefs: [userTypes],
+  resolvers: [userResolvers],
   formatError: (err) => {
     console.error(err);
     return err;
@@ -19,20 +20,27 @@ const server = new ApolloServer({
 });
 
 (async () => {
-  await connect();
-  const db = await getDB();
+  try {
+    console.log('Connecting to database');
+    await connect();
+    console.log('Connected to database');
+    await new Promise(resolve => setTimeout(resolve, 30000));
+    const db = await getDB();
 
-  const { url } = await startStandaloneServer(server, {
-    listen: process.env.PORT || 4000,
-    context: async ({ req, res }) => {
-      return {
-        auth: async () => {
-          return await authentication(req);
-        },
-        db,
-      };
-    },
-  });
+    const { url } = await startStandaloneServer(server, {
+      listen: { port: process.env.PORT || 4000 },
+      context: async ({ req, res }) => {
+        return {
+          auth: async () => {
+            return await authentication(req);
+          },
+          db,
+        };
+      },
+    });
 
-  console.log(`Server starting at ${url}`);
+    console.log(`Server starting at ${url}`);
+  } catch (error) {
+    console.error('Error starting server:', error);
+  }
 })();
--- server/config/mongo-connection.js
+++ server/config/mongo-connection.js
@@ -2,25 +2,27 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_CONN_STRING;
 const client = new MongoClient(uri);
 
-const DB_NAME = "pdrvmap24"
-let database
+const DB_NAME = "pdrvmap24";
+let database;
 
 async function connect() {
-	try {
-		await client.connect()
-		console.log(`MongoDB Connected..`);
-		database = client.db(DB_NAME);
-		return database;
-
-	} catch (error) {
-		console.log(`MongoDB Connection Error`);
-		await client.close();
-		throw error
-	}
+  try {
+    await client.connect();
+    console.log("MongoDB Connected..");
+    database = client.db(DB_NAME);
+    return database;
+  } catch (error) {
+    console.log("MongoDB Connection Error");
+    await client.close();
+    throw error;
+  }
 }
 
 function getDB() {
-	return database;
+  if (!database) {
+    throw new Error("Database not connected. Please call connect first.");
+  }
+  return database;
 }
 
 module.exports = { connect, getDB };

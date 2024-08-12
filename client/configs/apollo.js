88b025ceb40b212f63a1fec08359c7cbc87dd065
Alexander William <alexwilliam112@gmail.com>
2024-08-12 11:52:37 +0700

    client to prod

--- client/configs/apollo.js
+++ client/configs/apollo.js
@@ -10,7 +10,7 @@ if (__DEV__) {
 }
 
 const httpLink = createHttpLink({
-  uri: "https://9pn9pn4q-3004.asse.devtunnels.ms/",
+  uri: "https://vmpedro.alexanderwil.xyz",
 });
 
 const authLink = setContext(async (_, { headers }) => {
